429e5dd85533c6626bc55a4949a820557b4bd200
liqingkang <liqingkang@aobi.com>
2023-07-26 16:58:49 +0800

    add glm

--- Magic/src/Magic/Core/Application.cpp
+++ Magic/src/Magic/Core/Application.cpp
@@ -10,6 +10,14 @@
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_glfw.h>
 
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+
+
 namespace Magic {
 	Application::Application()
 	{
@@ -139,6 +147,14 @@ namespace Magic {
 
 			glUseProgram(program);
 			//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			float Translate = 0;
+			glm::vec2 Rotate(0,0);
+			glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+			glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+			View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+			View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+			glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
 
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
