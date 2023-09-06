8b8136db61a633494a8779c81bd246399424bde0
liqingkang <liqingkang@aobi.com>
2023-09-06 14:55:25 +0800

    add SPIR-V references

--- Magic/src/Magic/Graphics/Shader.cpp
+++ Magic/src/Magic/Graphics/Shader.cpp
@@ -4,6 +4,17 @@
 
 #include "Platform/OpenGL/OpenGLShader.h"
 
+/// <summary>
+/// SPIRV
+/// 1、Use libshaderc compiling shader strings into SPIR-V.
+/// https://github.com/google/shaderc/tree/main/libshaderc
+/// 2、Use SPIR-V binary cross comiple to other platform.
+/// https://github.com/KhronosGroup/SPIRV-Cross/tree/main
+/// 3、Some leaning references
+/// https://vulkan-tutorial.com/Drawing_a_triangle/Graphics_pipeline_basics/Shader_modules#page_Compiling-the-shaders
+/// 
+/// </summary>
+
 namespace Magic {
 	Ref<Shader> Shader::Create(std::string filePath) {
 		RendererAPI::API api = RendererAPI::GetAPI();
